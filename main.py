a7ddf029011865ea6b2c240256d29f4bebd8f942
JPlima21 <jplima595@gmail.com>
2024-08-04 17:11:58 -0300

    tratamento de erros

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, ElementNotSelectableException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, ElementNotSelectableException, TimeoutException, WebDriverException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -30,24 +30,48 @@ def get_weather(driver, wait):
     driver, wait = config_driver()
     url = 'https://www.accuweather.com/pt/world-weather'
 
-    driver.get(url)
-    sleep(1)
-    pesquisa = wait.until(EC.element_to_be_clickable((By.XPATH,'//input[@class="search-input"]')))
-    sleep(1)
-    pesquisa.send_keys('Fortaleza ceará' + Keys.ENTER)
-    sleep(1)
-    wait.until(EC.presence_of_all_elements_located((By.XPATH,'//div[@class="temp-container"]/div[@class="temp"]')))
-    wait.until(EC.presence_of_all_elements_located((By.XPATH,'//div[@class="real-feel"]')))
-    temp_atual = driver.find_element(By.XPATH,'//div[@class="temp-container"]/div[@class="temp"]')
-    sensacao_termica = driver.find_element(By.XPATH,'//div[@class="real-feel"]')
-    sensacao_texto = sensacao_termica.text
-    atual_texto = temp_atual.text
+    #Tratamento de erro
+    print('Acessando o site...')
+    try:
+        driver.get(url)
+        sleep(1)
+    except TimeoutException as e:
+        print(f'Erro ao acessar o site {url} : {e}')
+        return None
+    except WebDriverException as e:
+        print(f'Erro ao acessar o site {url} : {e}')
+        return None
+    
+    print('Pegando informações...')
+    try:
+        pesquisa = wait.until(EC.element_to_be_clickable((By.XPATH,'//input[@class="search-input"]')))
+        sleep(1)
+        pesquisa.send_keys('Fortaleza ceará' + Keys.ENTER)
+        sleep(1)
+        wait.until(EC.presence_of_all_elements_located((By.XPATH,'//div[@class="temp-container"]/div[@class="temp"]')))
+        wait.until(EC.presence_of_all_elements_located((By.XPATH,'//div[@class="real-feel"]')))
+        wait.until(EC.presence_of_all_elements_located((By.XPATH,'//span[@class="phrase"]')))
+        
+        #Pegando a temperatura atual, sensação termica e condição atual
+        temp_atual = driver.find_element(By.XPATH,'//div[@class="temp-container"]/div[@class="temp"]')
+        sensacao_termica = driver.find_element(By.XPATH,'//div[@class="real-feel"]')
+        condicao_temp = driver.find_element(By.XPATH,'//span[@class="phrase"]')
 
-    print(atual_texto)
-    print(sensacao_texto[-3:])
+        sensacao_texto = sensacao_termica.text
+        atual_texto = temp_atual.text
+        condicao_texto = condicao_temp.text
 
-    driver.quit()
+        print(condicao_texto)
+        print(f' temperatura atual: {atual_texto}')
+        print(sensacao_texto + 'C')
+    except:
+        print(f'Erro! elemento não encontrado')
 
+    finally:
+        driver.quit()
+
+def email():
+    pass
 
 def exe_script():
     driver, wait = config_driver()
